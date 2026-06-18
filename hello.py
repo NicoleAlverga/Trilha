e023fdeb94a46ea793bccc20a706d60284870124
Nicole Alverga <nicolesitarogusmao@gmail.com>
2026-06-17 23:33:10 -0300

    meu primeiro commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+lista = [1, 3, 5, 7, 9] 
+len(lista)
+print(len(lista))
+alto = len(lista)- 1
+baixo = len(lista) - 5
+print(alto)
+print(lista[3])
+print(lista[len(lista)- 2])
+print(baixo)
+print(lista[baixo])
+print(lista[4])
+print(lista[alto])
+print(lista[len(lista)- 1])
+print(lista[(alto+baixo)//2])
+print((alto+baixo)//2) 
+frase = "o céu está asul"
+correcao = frase.replace("asul", "azul")
+print(correcao)
+print(frase)
+nome = "Nicole de Alverga"
+print(nome.split())
+partes = nome.split()
+print(partes[0])
+
+idade = 21
+if idade >= 18 : 
+    print("liberado")
+else : 
+    print("proibido")
+
+
+
+
